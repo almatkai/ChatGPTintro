393e7dd4dd94da34427f15dac8eec7219004b749
almatkai <kairatovalmat2003@gmail.com>
2023-08-08 19:42:28 +0600

    Initial Commit

---
+++ ChatGPTintro/ChatGPTintroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTintroApp.swift
+//  ChatGPTintro
+//
+//  Created by Almat Kairatov on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTintroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
