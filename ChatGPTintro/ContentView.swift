8ab7da99711a9861e80a703760bdc2c251eafa2d
almatkai <kairatovalmat2003@gmail.com>
2023-08-09 11:23:54 +0600

    Added comments and explanations

--- ChatGPTintro/ContentView.swift
+++ ChatGPTintro/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // MARK: - Private Variables
     private let foregroundColors: [Color] = [.blue, .yellow, .red, .green, .purple, .orange, .pink, .teal, Color(uiColor: #colorLiteral(red: 0.04696316272, green: 0.06727208942, blue: 0.2167890072, alpha: 1))]
     private let backgroundColors: [Color] = [Color(uiColor: #colorLiteral(red: 0.1860292554, green: 0.17613253, blue: 0.1892024577, alpha: 1)), .gray, .cyan, .indigo, .mint, .red, .green, .yellow, .purple]
     
@@ -29,6 +30,7 @@ struct ContentView: View {
         "Problem Solve"
     ]
 
+    // MARK: - State Properties
     @State private var index = 8
     @State private var sloganInd = 0
     @State private var textLength: CGFloat? = nil
@@ -47,7 +49,8 @@ struct ContentView: View {
                         }
                         .frame(width: textLength)
                         .clipped()
-                        .offset(x: textLength == 0 ? 5 : -((textLength ?? 0) + 45)/2 - 5)
+                        // MARK: - Text offset depending text width -> -((textLength ?? 0) + 45)/2 - 5
+                        .offset(x: textLength == 0 ? 0 : -((textLength ?? 0) + 45)/2 - 5)
                     }
                     .offset(x: textLength == 0 ? 0 : ((textLength ?? 0) + 45)/2)
             }
@@ -67,14 +70,14 @@ struct ContentView: View {
         }
     private func intro() {
         Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { timer in
-            withAnimation(.interactiveSpring(response: 0.4, dampingFraction: 0.7, blendDuration: 0.9)) {
+            withAnimation(.easeInOut(duration: 0.3)) {
                 textLength = 0
             }
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
                 if textLength == 0 {
+                    // Prevent Duplicate
                     var oldIndex = index
-                    
                     repeat {
                         index = Int.random(in: 0..<foregroundColors.count)
                     } while index == oldIndex
@@ -84,7 +87,8 @@ struct ContentView: View {
                         sloganInd = Int.random(in: 0..<slogan.count)
                     } while sloganInd == oldIndex
 
-                    withAnimation(.interactiveSpring(response: 0.4, dampingFraction: 0.7, blendDuration: 0.7)) {
+                    // Resize to new text
+                    withAnimation(.easeInOut(duration: 0.3)) {
                         textSize(slogan[sloganInd])
                     }
                 }
